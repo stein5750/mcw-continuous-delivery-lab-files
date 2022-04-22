08f775b417126f6bbe45b38dbabd51368afcceaf
Stein <steinatcourse4@outlook.com>
2022-04-22 13:15:54 +0200

    added student prefix sby, set regionName to westerneurope and swedencentral, set isZoneRedundant for location2 to false.

--- infrastructure/deploy-infrastructure.ps1
+++ infrastructure/deploy-infrastructure.ps1
@@ -1,10 +1,10 @@
-$studentprefix = "Your 3 letter abbreviation here"
+$studentprefix = "sby"
 $resourcegroupName = "fabmedical-rg-" + $studentprefix
 $cosmosDBName = "fabmedical-cdb-" + $studentprefix
 $webappName = "fabmedical-web-" + $studentprefix
 $planName = "fabmedical-plan-" + $studentprefix
-$location1 = "westeurope"
-$location2 = "northeurope"
+$location1 = "norwayeast"
+$location2 = "swedencentral"
 
 # Create resource group
 az group create `
@@ -16,7 +16,7 @@ az cosmosdb create `
     --name $cosmosDBName `
     --resource-group $resourcegroupName `
     --locations regionName=$location1 failoverPriority=0 isZoneRedundant=False `
-    --locations regionName=$location2 failoverPriority=1 isZoneRedundant=True `
+    --locations regionName=$location2 failoverPriority=1 isZoneRedundant=False `
     --enable-multiple-write-locations `
     --kind MongoDB
 
